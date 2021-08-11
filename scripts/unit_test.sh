5f087c1139f05b21272fe3ec0caa968d6f57a47c
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-08-11 15:46:12 +0000

    removing random things to see what sticks

--- scripts/unit_test.sh
+++ scripts/unit_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #install dependencies
-#sudo apt-get install python3 python3-pip python3-venv -y
+sudo apt-get install python3 python3-pip python3-venv -y
 # create and activate venv
-python3 -m venv venv
-source venv/bin/activate
+# python3 -m venv venv
+# source venv/bin/activate
 # install reqs
 pip3 install -r requirements.txt
 #run tests with cov reports
