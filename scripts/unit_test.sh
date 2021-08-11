6a8b4446f5fc34ea14d6e7b08caeadca1a8015b9
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-08-11 15:07:19 +0000

    added pip instal after venv activate

--- scripts/unit_test.sh
+++ scripts/unit_test.sh
@@ -5,6 +5,7 @@ sudo apt-get install python3 python3-pip python3-venv -y
 # create and activate venv
 python3 -m venv venv
 source venv/bin/activate
+sudo apt-get install python3 python3-pip
 # install reqs
 pip3 install -r requirements.txt
 #run tests with cov reports
