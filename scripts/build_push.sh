426be642cad5c7b3fd8eab1f22cdd377c5d86f26
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-08-11 16:46:51 +0000

    updated build_push script

--- scripts/build_push.sh
+++ scripts/build_push.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-docker-compose build --parallel
-
 docker login -u ${DOCKERHUB_CREDENTIALS_USR} -p ${DOCKERHUB_CREDENTIALS_PSW}
+
+docker-compose build --parallel && \
 docker-compose push
 
