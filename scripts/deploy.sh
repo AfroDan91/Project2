0561095f4479d2c617e18e37b2a6ccdae14aaa29
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-08-12 14:55:27 +0000

    edited deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 ssh swarm-manager << EOF
 export DATABSE_URI=DATABASE_URI
-docker stack deploy --compose-file docker-compose.yaml BATTLE SIM
+docker stack deploy --compose-file docker-compose.yaml BATTLE_SIM
 EOF
