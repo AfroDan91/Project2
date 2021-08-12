e7fb2b0f96b233839fee3ec804669936fa6834f7
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-08-12 15:07:49 +0000

    added copy compose file step to deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,6 @@
+
+sudo scp ${WORKSPACE}/docker-compose.yaml jenkins@swarm-manager:/home/danie
+
 ssh swarm-manager << EOF
 export DATABSE_URI=DATABASE_URI
 docker stack deploy --compose-file docker-compose.yaml BATTLE_SIM
