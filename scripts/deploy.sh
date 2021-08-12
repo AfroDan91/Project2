763d9afc50aaaa2267cbf0aaef354e8ebeb59aa1
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-08-12 15:59:37 +0000

    edited deploy script8

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@ ls
 sudo scp /home/jenkins/Project2/docker-compose.yaml jenkins@swarm-manager:/home/jenkins
 
 ssh swarm-manager << EOF
-export DATABSE_URI=DATABASE_URI
+export DATABSE_URI=$DATABASE_URI
 docker stack deploy --compose-file docker-compose.yaml BATTLE_SIM
 EOF
