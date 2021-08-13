48b122e3345683ad1a74b146f8323d594a3c7755
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-08-13 10:17:50 +0000

    added external api call

--- service_3/app.py
+++ service_3/app.py
@@ -4,11 +4,12 @@ import random
 app = Flask(__name__)
 
 names = ["Steve the King", "Evan Almighty", "Thor"]
+response3 = requests.get("https://api.parser.name/?api_key=25d97a94cfff505ad3b2a732cf9b2496&endpoint=generate").json()
 
 @app.route('/get/pstats')
 def player_gen():
 
-    player_name = random.choice(names)
+    player_name = response3['data'][0]['name']['firstname']['name']
     attack = random.randint(1, 10)
     defence = random.randint(1, 10)
     pstats = [player_name, attack, defence]
