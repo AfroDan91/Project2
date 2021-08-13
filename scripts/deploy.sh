e3de36f14942925da0b0674c727d3893dd68b1b6
AfroDan91 <daniel_wordie@hotmail.co.uk>
2021-08-13 09:19:03 +0000

    fixed typo in db uri

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 rsync docker-compose.yaml swarm-manager:
 
 ssh swarm-manager << EOF
-export DATABSE_URI=$DATABASE_URI
+export DATABASE_URI=$DATABASE_URI
 docker stack deploy --compose-file docker-compose.yaml BATTLE_SIM
 EOF
